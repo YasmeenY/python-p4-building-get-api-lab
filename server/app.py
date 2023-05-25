c7fdbc65569c3db25c09e1e47b8617def3a87a1a
Yasmeen Yousef <yasmeen.yousef05@gmail.com>
2023-05-25 13:40:09 -0400

    lab complete

--- server/app.py
+++ server/app.py
@@ -20,19 +20,35 @@ def index():
 
 @app.route('/bakeries')
 def bakeries():
-    return ''
+    bakeries = Bakery.query.all()
+    bakery_dict = [bakery.to_dict() for bakery in bakeries]
+    response = make_response(bakery_dict, 200)
+    response.headers["content-Type"] = "application/json"
+    return response
 
 @app.route('/bakeries/<int:id>')
 def bakery_by_id(id):
-    return ''
+    bakery = Bakery.query.filter(Bakery.id == id).first()
+    bakery_dict = bakery.to_dict()
+    response = make_response(bakery_dict, 200)
+    response.headers["content-Type"] = "application/json"
+    return response
 
 @app.route('/baked_goods/by_price')
 def baked_goods_by_price():
-    return ''
+    goods_by_price = BakedGood.query.order_by(BakedGood.price).all()
+    goods_by_price_dict = [goods.to_dict() for goods in goods_by_price]
+    response = make_response(goods_by_price_dict, 200)
+    response.headers["content-Type"] = "application/json"
+    return response
 
 @app.route('/baked_goods/most_expensive')
 def most_expensive_baked_good():
-    return ''
+    goods_by_price = BakedGood.query.order_by(BakedGood.price.desc()).limit(1).first()
+    goods_dict = goods_by_price.to_dict()
+    response = make_response(goods_dict, 200)
+    response.headers["content-Type"] = "application/json"
+    return response
 
 if __name__ == '__main__':
     app.run(port=555, debug=True)
